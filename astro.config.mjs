9f98875204d7e17e51f2eda8335387cce95460d6
SabeeirSharrma <sabplayzthepro@gmail.com>
2026-06-19 15:12:08 +0530

    configure github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import mdx from '@astrojs/mdx'
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
+  site: "https://thecinderproject.qd.je",
+
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  integrations: [mdx()]
 });
